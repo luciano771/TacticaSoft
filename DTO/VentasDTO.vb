01c270139ba35059f71f45b42827afa2dcce9df2
luciano771 <79546157+luciano771@users.noreply.github.com>
2023-11-28 00:00:14 -0300

    se modificaron los formularios ventas y productos

---
+++ DTO/VentasDTO.vb
@@ -0,0 +1,46 @@
+﻿Namespace TacticaSoft.DTO
+    Public Class VentasDTO
+        Private _id As Integer
+        Private _idcliente As String
+        Private _fecha As String
+        Private _total As String
+
+        Public Property ID As Integer
+            Get
+                Return _id
+            End Get
+            Set(value As Integer)
+                _id = value
+            End Set
+        End Property
+
+        Public Property idcliente As String
+            Get
+                Return _idcliente
+            End Get
+            Set(value As String)
+                _idcliente = value
+            End Set
+        End Property
+
+        Public Property fecha As String
+            Get
+                Return _fecha
+            End Get
+            Set(value As String)
+                _fecha = value
+            End Set
+        End Property
+
+        Public Property fecha As String
+            Get
+                Return _total
+            End Get
+            Set(value As String)
+                _total = value
+            End Set
+        End Property
+
+    End Class
+
+End Namespace
---
+++ DTO/VentasItemDTO.vb
@@ -0,0 +1,66 @@
+﻿Namespace TacticaSoft.DTO
+    Public Class VentasitemDTO
+        Private _id As Integer
+        Private _idventa As String
+        Private _idproducto As String
+        Private _preciounitario As String
+        Private _cantidad As String
+        Private _preciototal As String
+
+        Public Property id As Integer
+            Get
+                Return _id
+            End Get
+            Set(value As Integer)
+                _id = value
+            End Set
+        End Property
+
+        Public Property idventa As String
+            Get
+                Return _idventa
+            End Get
+            Set(value As String)
+                _idventa = value
+            End Set
+        End Property
+
+        Public Property idproducto As String
+            Get
+                Return _idproducto
+            End Get
+            Set(value As String)
+                _idproducto = value
+            End Set
+        End Property
+
+        Public Property preciounitario As String
+            Get
+                Return _preciounitario
+            End Get
+            Set(value As String)
+                _preciounitario = value
+            End Set
+        End Property
+
+        Public Property cantidad As String
+            Get
+                Return _cantidad
+            End Get
+            Set(value As String)
+                _cantidad = value
+            End Set
+        End Property
+
+        Public Property preciototal As String
+            Get
+                Return _preciototal
+            End Get
+            Set(value As String)
+                _preciototal = value
+            End Set
+        End Property
+
+    End Class
+
+End Namespace
